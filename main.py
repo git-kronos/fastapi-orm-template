a19136119038698efb2ff8f3621531ebd6d5721b
bl-shuvamdas <shuvam.das@bridgelabz.com>
2023-02-07 20:05:45 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fastapi.testclient import TestClient
 
 from routes.users import route as user_routes
 
 app = FastAPI()
+client = TestClient(app=app)
 
 
 @app.get('/')
